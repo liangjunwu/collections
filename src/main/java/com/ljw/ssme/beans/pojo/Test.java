82af984a8446e53c0a6d89486b4ebb596c47f7f9
liangjunwu@aspirecn.com <liangjunwu@aspirecn.com>
2018-02-28 17:18:18 +0800

    1.0.1

---
+++ src/main/java/com/ljw/ssme/beans/pojo/Test.java
@@ -0,0 +1,13 @@
+package com.ljw.ssme.beans.pojo;
+
+public class Test {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
